3aea6bafa45dca98382939008726d2381ea74297
Ghanshyam Agrawal <ghanshyamagrawal456@gmail.com>
2024-03-19 10:21:05 +0530

    change

--- bliss/settings.py
+++ bliss/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%58sue2x*-u&yjdpfnvbs69#pmb3t(r!rqsax3h89lsytekiyg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
